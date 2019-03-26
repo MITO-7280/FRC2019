5659451572adb956df737c0e942b649de1d04341
Antiarisxu <jimmyxurs@gmail.com>
2019-03-26 21:07:05 +0800

    Update Vision.py

--- Vision/Vision.py
+++ Vision/Vision.py
@@ -78,10 +78,10 @@ cvBallServer = cs.MjpegServer("vision", 8082)
 cvBallServer.setSource(cvBallSource)
 print("OpenCV output ball server listening at http://0.0.0.0:8082")
 
-cvGroundSource = cs.CvSource("cvgroundsource", cs.VideoMode.PixelFormat.kMJPEG, width, height, 30)
-cvGroundServer = cs.MjpegServer("vision", 8182)
-cvGroundServer.setSource(cvGroundSource)
-print("OpenCV output ground server listening at http://0.0.0.0:8182")
+# cvGroundSource = cs.CvSource("cvgroundsource", cs.VideoMode.PixelFormat.kMJPEG, width, height, 30)
+# cvGroundServer = cs.MjpegServer("vision", 8182)
+# cvGroundServer.setSource(cvGroundSource)
+# print("OpenCV output ground server listening at http://0.0.0.0:8182")
 
 
 # keep looping
@@ -140,6 +140,8 @@ while True:
         ballPos = 0
     print("ballPos=", ballPos)
     ballNetwork.putNumber("Y", ballPos)
+
+    cvBallSource.putFrame(ballFrame)
     # update the points queuec
     # pts.appendleft(center)
     # loop over the set of tracked points
@@ -154,95 +156,95 @@ while True:
 
     # Tape Part
 
-    _, tapeFrame = ballSink.grabFrame(tapenp)
-    # tapeBlurred = cv2.blur(tapeFrame, (10, 10))
-    tapeHsv = cv2.cvtColor(tapeFrame, cv2.COLOR_BGR2HSV)
-    tapeMask = cv2.inRange(tapeHsv, hsvTapeLower, hsvTapeUpper)
-    tapeMask = cv2.erode(tapeMask, None, iterations=2)
-    tapeMask = cv2.dilate(tapeMask, None, iterations=2)
-    _, tapeContours, _ = cv2.findContours(tapeMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
-    # Copies frame and stores it in image
-    tapeImage = tapeFrame.copy()
-    # Processes the contours, takes in (contours, output_image, (centerOfImage)
-
-    if len(tapeContours) > 1:
-        cntsSorted = sorted(tapeContours, key=lambda x: cv2.contourArea(x), reverse=True)
-        rect0 = cv2.minAreaRect(cntsSorted[0])
-        box0 = cv2.boxPoints(rect0)
-        box0 = np.int0(box0)
-        # cv2.drawContours(ballFrame, [box0], 0, (0, 0, 255), 2)
-        rect1 = cv2.minAreaRect(cntsSorted[1])
-        box1 = cv2.boxPoints(rect1)
-        box1 = np.int0(box1)
-        # cv2.drawContours(ballFrame, [box1], 0, (0, 0, 255), 2)
-
-        tape0Center = int(rect0[0][0]), int(rect0[0][1])
-        tape1Center = int(rect1[0][0]), int(rect1[0][1])
-
-        tapeMiddleCenter0 = (tape0Center[0] + tape1Center[0]) / 2
-        tapeMiddleCenter1 = (tape0Center[1] + tape1Center[1]) / 2
-
-        if tapeMiddleCenter0 < (width / 2 - width / 20):
-            tapePos = 1
-        elif tapeMiddleCenter0 > (width / 2 + width / 20):
-            tapePos = 2
-        else:
-            tapePos = 3
-        # cv2.circle(ballFrame, (int(tapeMiddleCenter0), int(tapeMiddleCenter1)), 5, (0, 0, 255), -1)
-    else:
-        tapePos = 0
-    tapeNetwork.putNumber("X", tapePos)
+    # _, tapeFrame = ballSink.grabFrame(tapenp)
+    # # tapeBlurred = cv2.blur(tapeFrame, (10, 10))
+    # tapeHsv = cv2.cvtColor(tapeFrame, cv2.COLOR_BGR2HSV)
+    # tapeMask = cv2.inRange(tapeHsv, hsvTapeLower, hsvTapeUpper)
+    # tapeMask = cv2.erode(tapeMask, None, iterations=2)
+    # tapeMask = cv2.dilate(tapeMask, None, iterations=2)
+    # _, tapeContours, _ = cv2.findContours(tapeMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
+    # # Copies frame and stores it in image
+    # tapeImage = tapeFrame.copy()
+    # # Processes the contours, takes in (contours, output_image, (centerOfImage)
     #
-    cvBallSource.putFrame(ballFrame)
+    # if len(tapeContours) > 1:
+    #     cntsSorted = sorted(tapeContours, key=lambda x: cv2.contourArea(x), reverse=True)
+    #     rect0 = cv2.minAreaRect(cntsSorted[0])
+    #     box0 = cv2.boxPoints(rect0)
+    #     box0 = np.int0(box0)
+    #     # cv2.drawContours(ballFrame, [box0], 0, (0, 0, 255), 2)
+    #     rect1 = cv2.minAreaRect(cntsSorted[1])
+    #     box1 = cv2.boxPoints(rect1)
+    #     box1 = np.int0(box1)
+    #     # cv2.drawContours(ballFrame, [box1], 0, (0, 0, 255), 2)
+    #
+    #     tape0Center = int(rect0[0][0]), int(rect0[0][1])
+    #     tape1Center = int(rect1[0][0]), int(rect1[0][1])
+    #
+    #     tapeMiddleCenter0 = (tape0Center[0] + tape1Center[0]) / 2
+    #     tapeMiddleCenter1 = (tape0Center[1] + tape1Center[1]) / 2
+    #
+    #     if tapeMiddleCenter0 < (width / 2 - width / 20):
+    #         tapePos = 1
+    #     elif tapeMiddleCenter0 > (width / 2 + width / 20):
+    #         tapePos = 2
+    #     else:
+    #         tapePos = 3
+    #     # cv2.circle(ballFrame, (int(tapeMiddleCenter0), int(tapeMiddleCenter1)), 5, (0, 0, 255), -1)
+    # else:
+    #     tapePos = 0
+    # tapeNetwork.putNumber("X", tapePos)
+    # #
+    # cvBallSource.putFrame(ballFrame)
 
     # print(tapePos)
 
     # Ground Part
-    isNeeded = int(needNetwork.getNumber("X", 1))
-    if isNeeded == 1:
-        _, groundFrame = groundSink.grabFrame(tapenp)
-        groundBlurred = cv2.blur(groundFrame, (4, 4))
-        groundHsv = cv2.cvtColor(groundFrame, cv2.COLOR_BGR2HSV)
-        groundMask = cv2.inRange(groundHsv, hsvGroundLower, hsvGroundUpper)
-        groundMask = cv2.erode(groundMask, None, iterations=2)
-        groundMask = cv2.dilate(groundMask, None, iterations=2)
-        _, groundContours, _ = cv2.findContours(groundMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
-        groundImage = groundFrame.copy()
-        global groundPos
-        if len(groundContours) > 0:
-            groundc = max(groundContours, key=cv2.contourArea)
-            groundImage = cv2.drawContours(groundImage, [groundc], 0, (0, 255, 0), 2)
-            if cv2.contourArea(groundc) > 200:
-                groundM = cv2.moments(groundc)
-                groundCenter = (int(groundM["m10"] / groundM["m00"]), int(groundM["m01"] / groundM["m00"]))
-                cv2.circle(groundImage, groundCenter, 5, (0, 0, 255), -1)
-
-
-                rows, cols = groundImage.shape[:2]
-                [vx, vy, x, y] = cv2.fitLine(groundc, cv2.DIST_L2, 0, 0.01, 0.01)
-                k = vy / vx
-                lefty = int((-x * k) + y)
-                righty = int(((cols - x) * k) + y)
-                groundImage = cv2.line(groundImage, (cols - 1, righty), (0, lefty), (0, 255, 0), 2)
-
-                if groundCenter[0] < (width / 2 - width / 8):
-                    groundPos = 4
-                elif groundCenter[0] > (width / 2 + width / 8):
-                    groundPos = 5
-                elif (vx / vy) < (-1.732):
-                    groundPos = 1
-                elif (vx / vy) > 1.732:
-                    groundPos = 2
-                else:
-                    groundPos = 3
-            else:
-                groundPos = 6
-        else:
-            groundPos = 6
-        # print(groundPos)
-        tapeNetwork.putNumber("Y", groundPos)
-
-        cvGroundSource.putFrame(groundImage)
+    # isNeeded = int(needNetwork.getNumber("X", 1))
+    # if isNeeded == 1:
+    #     _, groundFrame = groundSink.grabFrame(tapenp)
+    #     groundBlurred = cv2.blur(groundFrame, (4, 4))
+    #     groundHsv = cv2.cvtColor(groundFrame, cv2.COLOR_BGR2HSV)
+    #     groundMask = cv2.inRange(groundHsv, hsvGroundLower, hsvGroundUpper)
+    #     groundMask = cv2.erode(groundMask, None, iterations=2)
+    #     groundMask = cv2.dilate(groundMask, None, iterations=2)
+    #     _, groundContours, _ = cv2.findContours(groundMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
+    #     groundImage = groundFrame.copy()
+    #     global groundPos
+    #     if len(groundContours) > 0:
+    #         groundc = max(groundContours, key=cv2.contourArea)
+    #         groundImage = cv2.drawContours(groundImage, [groundc], 0, (0, 255, 0), 2)
+    #         if cv2.contourArea(groundc) > 200:
+    #             groundM = cv2.moments(groundc)
+    #             groundCenter = (int(groundM["m10"] / groundM["m00"]), int(groundM["m01"] / groundM["m00"]))
+    #             cv2.circle(groundImage, groundCenter, 5, (0, 0, 255), -1)
+    #
+    #
+    #             rows, cols = groundImage.shape[:2]
+    #             [vx, vy, x, y] = cv2.fitLine(groundc, cv2.DIST_L2, 0, 0.01, 0.01)
+    #             k = vy / vx
+    #             lefty = int((-x * k) + y)
+    #             righty = int(((cols - x) * k) + y)
+    #             groundImage = cv2.line(groundImage, (cols - 1, righty), (0, lefty), (0, 255, 0), 2)
+    #
+    #             if groundCenter[0] < (width / 2 - width / 8):
+    #                 groundPos = 4
+    #             elif groundCenter[0] > (width / 2 + width / 8):
+    #                 groundPos = 5
+    #             elif (vx / vy) < (-1.732):
+    #                 groundPos = 1
+    #             elif (vx / vy) > 1.732:
+    #                 groundPos = 2
+    #             else:
+    #                 groundPos = 3
+    #         else:
+    #             groundPos = 6
+    #     else:
+    #         groundPos = 6
+    #     # print(groundPos)
+    #     tapeNetwork.putNumber("Y", groundPos)
+    #
+    #     cvGroundSource.putFrame(groundImage)
 
     FPS = 1 / (time.time() - tempTime)  # type: float
     print("FPS=", FPS) 
