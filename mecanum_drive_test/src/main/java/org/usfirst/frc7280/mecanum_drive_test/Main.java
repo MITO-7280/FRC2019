f210ec8b2e22a39e7e4ed6418faaf52fa0cc3005
Joey Zhou <2539292563@qq.com>
2022-01-17 22:46:02 +0800

    Update Main.java

--- mecanum_drive_test/src/main/java/org/usfirst/frc7280/mecanum_drive_test/Main.java
+++ mecanum_drive_test/src/main/java/org/usfirst/frc7280/mecanum_drive_test/Main.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj.RobotBase;
 public final class Main {
   private Main() {
   }
-
+// Testing for Github desktop
   /**
    * Main initialization function. Do not perform any initialization here.
    *
